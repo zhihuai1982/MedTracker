ea0beb4215b4272609aa22b0a97b19f954f6519f
zhihuai1982 <zhihuai1982@gmail.com>
2023-11-09 15:36:19 +0800

    修改post参数，取消验证

--- patientList.py
+++ patientList.py
@@ -3,7 +3,7 @@
 # %%
 
 import requests
-import json
+import re
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36"}
@@ -42,15 +42,18 @@ response = requests.request(
     "GET",
     url,
     headers=headers,
-    params=query
+    params=query,
+    verify = False
 )
 
 # 从response.json()获得的list中保留id,name,并排除name为"test"的项目,将剩余的项目的name值按"-"分解并获取第三项,保留为键值"mrn"
+
+pattern = r'^[A-Za-z0-9]+-[\u4e00-\u9fa5]+-\d+-.*$'
+
 tpList = [{key: d[key] for key in ['id', 'name']}
-          for d in response.json() if d['name'] != "test"]
+          for d in response.json() if re.match(pattern, d['name'])]
 for item in tpList:
     item['mrn'] = int(item['name'].split('-')[2])
-
 
 # 根据mrn同步tpList和hpList
 # 如果hpList中有tpList中没有的,则增加一项,将hplist的pinfo项赋值给tplist的name项,如果tpList中有hpList中没有的,则删除这一项
@@ -60,6 +63,7 @@ for item in tpList:
 # 在这个代码块中，`if not any(d['mrn'] == item['mrn'] for d in hpList)`的意思是：如果在`hpList`中没有任何一个元素的`mrn`属性与当前的`item`元素的`mrn`属性相等，则执行下面的代码块。如果存在一个或多个元素的`mrn`属性与当前的`item`元素的`mrn`属性相等，则不执行下面的代码块。
 # 换句话说，这个条件语句用于检查`hpList`中是否存在一个与当前`item`元素具有相同`mrn`属性的元素。如果不存在，则执行下面的代码块，否则不执行。
 
+# %% 
 id = "65296c002df7c2c909517c4e"
 
 
@@ -69,7 +73,8 @@ for item in hpList:
             "POST",
             f"https://api.trello.com/1/boards/{id}/lists",
             headers=headers,
-            params=dict({"name":item['pinfo']},**query)
+            params=dict({"name":item['pinfo']},**query),
+            verify = False,
         )
         print("add "+item['pinfo'])
     else:
@@ -78,7 +83,8 @@ for item in hpList:
                 requests.request(
                     "PUT",
                     f"https://api.trello.com/1/lists/{d['id']}",
-                    params=dict({"name":item['pinfo']},**query)
+                    params=dict({"name":item['pinfo']},**query),
+                    verify = False,
                 )
                 print("rename " + item['pinfo'])
 for item in tpList:
@@ -86,6 +92,8 @@ for item in tpList:
         requests.request(
             "PUT",
             f"https://api.trello.com/1/lists/{item['id']}/closed",
-            params=dict({'value':"true"},**query)
+            params=dict({'value':"true"},**query),
+            verify = False,
         )
         print("del " + item['name'])
+# %%
