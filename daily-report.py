94d582c94883695e2cec003e2a0cc22a5d2eb16c
zhihuai1982 <zhihuai1982@gmail.com>
2025-05-02 21:29:03 +0800

    每日汇报

--- daily-report.py
+++ daily-report.py
@@ -115,6 +115,9 @@ pList = pList.sort_values(by="sort_key").drop(columns=["sort_key"])
 
 pContent = ""  # 选择要发布的内容，drgs或appointment
 
+pContent += "<!-- wp:heading {'level':1} -->\n<h1 class='wp-block-heading'>DRGS 数据</h1>\n<!-- /wp:heading -->\n"
+
+
 for index, row in pList.iterrows():
     # 获取病历文书列表
     # http://20.21.1.224:5537/api/api/EmrWd/GetDocumentList/{mrn}/{series}/emr
@@ -185,8 +188,9 @@ for index, row in pList.iterrows():
 
         pContent += (
             f"<b>DRGS：</b><br>"
-            f"drgCode:{forecast.get('drgCode', '')}<br>"
+            f"drgCode：{forecast.get('drgCode', '')}<br>"
             f"DRG分组：{forecast.get('drgName', '')}<br>"
+            f"医疗总费用：{forecast.get('ylzfy', '')}<br>"
             f"预计结算金额：{forecast.get('feeSettle', 0)}<br>"
             f"预计结余：{forecast.get('feeProfit', 0)}<br>"
             f"类型：{forecast.get('caseTypeName', '')}<br>"
@@ -204,9 +208,141 @@ for index, row in pList.iterrows():
     if surgery_cost:
         pContent += f"手术费总额：{surgery_cost} 元<br>"
 
-print(pContent)
 
 # %%
+# 新增两周前后的日期变量
+two_weeks_ago_str = (datetime.date.today() - datetime.timedelta(days=14)).strftime(
+    "%Y-%m-%d"
+)
+two_weeks_later_str = (datetime.date.today() + datetime.timedelta(days=14)).strftime(
+    "%Y-%m-%d"
+)
+
+
+appointment_patients = requests.get(
+    f"http://20.21.1.224:5537/api/api/Public/GetCadippatientnoticelist/1/{two_weeks_ago_str}/{two_weeks_later_str}/5/33A/"
+).json()
+
+inpatient_patients = requests.get(
+    f"http://20.21.1.224:5537/api/api/Public/GetCadippatientnoticelist/1/{two_weeks_ago_str}/{two_weeks_later_str}/7/33A/"
+).json()
+# %%
+
+# 合并两个列表并转换为DataFrame
+combined_list = appointment_patients + inpatient_patients
+patient_df = pd.DataFrame(combined_list)[
+    [
+        "PatientName",
+        "PatientID",
+        "Isroom",
+        "Diagnose",
+        "drremark",
+        "PatientSex",
+        "PatientAge",
+        "Attending",
+        "Doctor",
+        "NoticeFlag",
+        "noticeRecord",
+        "AppointmentIn",
+        "AppOperativeDate",
+        "ApplicationDate",
+        "arrangedate",
+        "dohoscode",
+        "PatientPhone",
+    ]
+]
+
+# 删除NoticeFlag为"取消"的行
+patient_df = patient_df[patient_df["NoticeFlag"] != "取消"]
+
+
+# %%
+
+name_mapping = {
+    "30044": "胡孙宏",
+    "30046": "李文雅",
+    "30047": "周明光",
+    "30259": "肖芒",
+    "30274": "邵瑾燕",
+    "30291": "李旋",
+    "30295": "姜晓华",
+    "30346": "姜秀文",
+    "30402": "张丹a",
+    "30594": "张守德",
+    "30705": "冯佳鹏",
+    "30920": "帅冲",
+    "30949": "周森浩",
+    "31122": "党慧",
+    "31123": "李洁",
+    "31135": "金晟曦",
+    "73062": "赵丽娜",
+    "73145": "刘贝娜",
+    "73272": "周国金",
+    "73298": "董志怀",
+    "73403": "齐杰",
+    "73432": "潘虹c",
+    "73498": "项轩",
+    "73887": "岑威",
+    "3X020": "张京娜",
+    "3X051": "秦晨",
+    "3X118": "盖寅哲",
+    "3X217": "叶高飞",
+    "3X218": "金茂",
+}
+
+# 转换Attending和Doctor列为姓名
+patient_df["Attending"] = patient_df["Attending"].astype(str).map(name_mapping)
+patient_df["Doctor"] = patient_df["Doctor"].astype(str).map(name_mapping)
+
+
+# 修改统计逻辑为分层统计
+attending_stats = patient_df.groupby("Attending")
+
+# %%
+
+pContent += "<!-- wp:heading {'level':1} -->\n<h1 class='wp-block-heading'><br>每日预约病例</h1>\n<!-- /wp:heading -->\n"
+
+
+# combined_df 中的 ApplicationDate 格式为"2025-03-17T08:11:44", 筛选出当天日期的行
+today = datetime.date.today().strftime("%Y-%m-%d")
+# today = (datetime.date.today() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+today_patients = patient_df[patient_df["ApplicationDate"].str.startswith(today)]
+
+# 使用today_rows代替combined_df进行当日统计
+attending_stats_today = today_patients.groupby("Attending")
+for attending, group in attending_stats_today:
+    pContent += f"<br>================================<br>==  {attending}（{len(group)}）<br>================================<br><br>"
+
+    # 统计逻辑保持相同结构
+    doctor_counts = group.groupby("Doctor").size()
+    diagnosis_counts = group.groupby(["Doctor", "Diagnose"]).size()
+
+    for doctor in doctor_counts.index:
+        pContent += f"<b>{doctor} - {doctor_counts[doctor]}</b><br>"
+        diag_counts = diagnosis_counts.xs(doctor, level="Doctor")
+        for diag, count in diag_counts.items():
+            pContent += f"┗ {diag} - {count}<br>"
+
+pContent += "<!-- wp:heading {'level':1} -->\n<h1 class='wp-block-heading'><br>月度预约病例</h1>\n<!-- /wp:heading -->\n"
+
+
+for attending, group in attending_stats:
+    pContent += f"<br>================================<br>==  {attending}（{len(group)}）<br>================================<br><br>"
+
+    # 先统计医生总数
+    doctor_counts = group.groupby("Doctor").size()
+    # 再统计每个医生的诊断分布
+    diagnosis_counts = group.groupby(["Doctor", "Diagnose"]).size()
+
+    # 修改循环为按数量降序输出
+    for doctor in doctor_counts.sort_values(ascending=False).index:  # 新增排序
+        # 输出医生总数
+        pContent += f"<b>{doctor} - {doctor_counts[doctor]}</b><br>"
+        # 输出该医生的诊断分布
+        diag_counts = diagnosis_counts.xs(doctor, level="Doctor")
+        for diag, count in diag_counts.items():
+            pContent += f"┗ {diag} - {count}<br>"
+
 
 # %%
 # 新增WordPress发布模块
